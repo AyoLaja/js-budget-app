7f80ece6f5ac4c3e69bedfaf1ff4eec1d0988dda
Ayomide Onalaja <ayo.onalaja@gmail.com>
2019-01-11 19:57:37 -0500

    budget input done

--- js-budget-setup-master/js/app.js
+++ js-budget-setup-master/js/app.js
@@ -15,6 +15,63 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  //Submit budget method
+  submitBudget() {
+    const value = this.budgetInput.value
+
+    if (value === '' || value <= 0) {
+      this.budgetFeedback.innerHTML = `<span>Budget cannot be empty, zero or negative</span>`
+      this.budgetFeedback.classList.remove('hide-feedback')
+      this.budgetFeedback.classList.add('show-feedback', 'error-feedback')
+
+      setTimeout(() => {
+        this.budgetFeedback.classList.remove('show-feedback', 'error-feedback')
+        this.budgetFeedback.classList.add('hide-feedback')
+      }, 2000)
+    }
+    else {
+      // this.budgetAmount.textContent = value
+      // this.budgetInput.value = ''
+      // this.showBalance()
+
+      var balanceContainer = document.querySelector('.balance-container');
+      balanceContainer.classList.add('slide-left-center')
+
+      var budgetContainer = document.querySelector('.budget-container');
+      budgetContainer.classList.remove('slide-left-center')
+      budgetContainer.classList.add('slide-left-out')
+    }
+  }
+
+  showBalance() {
+
+  }
+}
+
+function eventListeners() {
+  const budgetForm = document.querySelector('#budget-form')
+  const expenseForm = document.querySelector('#expense-form')
+  const expenseList = document.querySelector('#expense-list')
+
+  //Create ne UI instance
+  const bForBudget = new UI()
+
+  //Budget form submit
+  budgetForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    bForBudget.submitBudget()
+  })
+
+  //Expense form submit
+  expenseForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+  })
+
+  //Expense list click
+  expenseList.addEventListener('click', function() {
+    
+  })
 }
 
 var nextButton = document.querySelector('#welcome-next')
@@ -27,6 +84,9 @@ function next(event) {
   welcomeMessage.classList.add('slide-left-out')
 
   var budgetContainer = document.querySelector('.budget-container');
-  // budgetForm.style.display = 'block'
   budgetContainer.classList.add('slide-left-center')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListeners()
+})
